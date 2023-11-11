bcc5b25d7e546e1d73b7bb89216c00aa3a17a47c
urswilke <urs.wilke@gmail.com>
2023-11-11 02:41:27 +0100

    copy file playwright.yml workflow for gha from here: https://github.com/urswilke/lit_gha/

--- test/simple.test.js
+++ test/simple.test.js
@@ -1,6 +1,7 @@
 import {OJSPlot} from '../src/ojs-plot.js';
 import {assert} from '@open-wc/testing';
 
+
 suite('simple test', () => {
   test('is defined', () => {
     const el = document.createElement('ojs-plot');
